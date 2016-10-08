1b8d5d2587e0c065661f39b023bbd489f46b6d74
sujoyu <sujoyu.motoki@gmail.com>
2016-10-08 20:19:23 +0900

    iOS10でタップが効かない問題の対処

---
+++ js/for-ios10.js
@@ -0,0 +1,28 @@
+(function() {
+  function ios_ver (){
+    var ios_ua = navigator.userAgent;
+    if( ios_ua.indexOf("iPhone") > 0 ) {
+      ios_ua.match(/iPhone OS (\w+)/g);
+      var version = RegExp.$1;
+      return version;
+    }
+  }
+  if (ios_ver() === '10_0') {
+    window.__Touch = window.Touch;
+    window.Touch = function(init) {
+      this.target = init.target;
+      this.identifier = init.identifier;
+      this.clientX = init.clientX;
+      this.clientY = init.clientY;
+      this.pageX = init.pageX;
+      this.pageY = init.pageY;
+      this.screenX = init.screenX;
+      this.screenY = init.screenY;
+    }
+    window.Touch.prototype = Object.create(window.__Touch.prototype);
+    window.Touch.prototype.constructor = window.Touch;
+
+    window.__TouchEvent = window.TouchEvent;
+    window.TouchEvent = null;
+  }
+})();
